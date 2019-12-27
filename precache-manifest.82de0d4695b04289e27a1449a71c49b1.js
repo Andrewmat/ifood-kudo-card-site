cd9c7530b1b40da8ec060f142925ccfb0ef3aa5b
andre-matulionis-ifood <andre.matulionis@ifood.com.br>
2019-12-27 13:45:41 -0300

    V1

---
+++ precache-manifest.82de0d4695b04289e27a1449a71c49b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "140539f32a58c1a429d77b81cd1566c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff13e6dca6a700cb575d",
+    "url": "/static/css/main.49fe57dc.chunk.css"
+  },
+  {
+    "revision": "87bdbc3f493a9fa0881d",
+    "url": "/static/js/2.e4c49afb.chunk.js"
+  },
+  {
+    "revision": "df9ead2900c74c3c021b73393b2ac3dd",
+    "url": "/static/js/2.e4c49afb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ff13e6dca6a700cb575d",
+    "url": "/static/js/main.e76bd636.chunk.js"
+  },
+  {
+    "revision": "0b0984da127ffca65c20",
+    "url": "/static/js/runtime-main.08b2cc9a.js"
+  }
+]);
